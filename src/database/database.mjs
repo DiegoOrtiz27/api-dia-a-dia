b73e166f2a61e2fe68f426a0f16904d39d9baadb
Diego Ortiz <emanuelortizlopez@gmail.com>
2022-08-11 20:25:47 -0500

    change require mysql

--- src/database/database.mjs
+++ src/database/database.mjs
@@ -8,6 +8,6 @@ const connection = mysql.createConnection({
     password: config.password
 });
 
-export default getConnection = () => {
+export function getConnection() {
     return connection;
 };
